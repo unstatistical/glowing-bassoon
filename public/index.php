31e8e9b969054ef1a58b41bbf1e1c94c3f6b182c
Desmond Vasicek <desmond.vasicek@protonmail.com>
2026-04-27 16:51:37 -0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@
 <!-- You should have received a copy of the GNU General Public License       -->
 <!-- along with this program.  If not, see <https://www.gnu.org/licenses/>.  -->
 
-<!DOCTYPE html>'
+<!DOCTYPE html>
 
 <html lang="en">
     <head>
