6ab25d4e3bcad394d30c8ba33c4b72d8db3e5373
Desmond Vasicek <desmond.vasicek@protonmail.com>
2026-04-27 16:20:25 -0700

    Move PHP Socker Configuration to www.conf

--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>index.php</title>
+    </head>
+    <body>
+        <header></header>
+        <main></main>
+        <footer></footer>
+    </body>
+</html>
